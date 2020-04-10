e15c0529a4f6bb7c661317aa188fdb502f485600
PrimosK <primoz.kokol@gmail.com>
2020-04-10 07:12:27 +0200

    Replaced tabs with spaces.

--- gemsfx/src/main/java/com/dlsc/gemsfx/util/PdfBoxDocument.java
+++ gemsfx/src/main/java/com/dlsc/gemsfx/util/PdfBoxDocument.java
@@ -22,168 +22,168 @@ import java.util.List;
 
 public class PdfBoxDocument implements PDFView.SearchableDocument {
 
-	private PDDocument document;
+   private PDDocument document;
 
-	public PdfBoxDocument(InputStream pdfInputStream) {
-		try {
-			document = PDDocument.load(pdfInputStream);
-		}
-		catch (IOException e) {
-			throw new DocumentProcessingException(e);
-		}
-	}
+   public PdfBoxDocument(InputStream pdfInputStream) {
+      try {
+         document = PDDocument.load(pdfInputStream);
+      }
+      catch (IOException e) {
+         throw new DocumentProcessingException(e);
+      }
+   }
 
-	public PdfBoxDocument(File file) {
-		try {
-			document = PDDocument.load(file);
-		}
-		catch (IOException e) {
-			throw new DocumentProcessingException(e);
-		}
-	}
+   public PdfBoxDocument(File file) {
+      try {
+         document = PDDocument.load(file);
+      }
+      catch (IOException e) {
+         throw new DocumentProcessingException(e);
+      }
+   }
 
-	@Override
-	public int getNumberOfPages() {
-		return document.getNumberOfPages();
-	}
+   @Override
+   public int getNumberOfPages() {
+      return document.getNumberOfPages();
+   }
 
-	@Override
-	public boolean isLandscape(int pageNumber) {
-		final PDPage page = document.getPage(pageNumber);
-		final PDRectangle cropBox = page.getCropBox();
-		return cropBox.getHeight() < cropBox.getWidth();
-	}
+   @Override
+   public boolean isLandscape(int pageNumber) {
+      final PDPage page = document.getPage(pageNumber);
+      final PDRectangle cropBox = page.getCropBox();
+      return cropBox.getHeight() < cropBox.getWidth();
+   }
 
-	@Override
-	public BufferedImage renderPage(int pageNumber, float scale) {
-		final PDFRenderer renderer = new PDFRenderer(document);
-		final BufferedImage bufferedImage;
+   @Override
+   public BufferedImage renderPage(int pageNumber, float scale) {
+      final PDFRenderer renderer = new PDFRenderer(document);
+      final BufferedImage bufferedImage;
 
-		try {
-			bufferedImage = renderer.renderImage(pageNumber, scale, ImageType.ARGB, RenderDestination.VIEW);
-		}
-		catch (IOException e) {
-			throw new DocumentProcessingException(e);
-		}
+      try {
+         bufferedImage = renderer.renderImage(pageNumber, scale, ImageType.ARGB, RenderDestination.VIEW);
+      }
+      catch (IOException e) {
+         throw new DocumentProcessingException(e);
+      }
 
-		return bufferedImage;
-	}
+      return bufferedImage;
+   }
 
-	@Override
-	public List<PDFView.SearchResult> getSearchResults(String searchText) {
+   @Override
+   public List<PDFView.SearchResult> getSearchResults(String searchText) {
 
-		final List<PDFView.SearchResult> results = new ArrayList<>();
+      final List<PDFView.SearchResult> results = new ArrayList<>();
 
-		final PDFTextStripper stripper;
+      final PDFTextStripper stripper;
 
-		try {
-			stripper = new PDFTextStripper() {
+      try {
+         stripper = new PDFTextStripper() {
 
-				private int pageNumber = -1;
+            private int pageNumber = -1;
 
-				@Override
-				protected void startPage(PDPage page) {
-					pageNumber++;
-				}
+            @Override
+            protected void startPage(PDPage page) {
+               pageNumber++;
+            }
 
-				@Override
-				protected void writeString(String text, List<TextPosition> textPositions) {
-					if (StringUtils.containsIgnoreCase(text, searchText)) {
-						PDFView.SearchResult
-								  result = new PDFView.SearchResult(searchText, text, pageNumber, calculateMarkerPosition(searchText, text, textPositions));
-						results.add(result);
-					}
-				}
-			};
-		}
-		catch (IOException e) {
-			throw new DocumentProcessingException(e);
-		}
+            @Override
+            protected void writeString(String text, List<TextPosition> textPositions) {
+               if (StringUtils.containsIgnoreCase(text, searchText)) {
+                  PDFView.SearchResult
+                          result = new PDFView.SearchResult(searchText, text, pageNumber, calculateMarkerPosition(searchText, text, textPositions));
+                  results.add(result);
+               }
+            }
+         };
+      }
+      catch (IOException e) {
+         throw new DocumentProcessingException(e);
+      }
 
-		Writer writer = new Writer() {
+      Writer writer = new Writer() {
 
-			@Override
-			public void write(char[] cbuf, int off, int len) {
-				// Do nothing
-			}
+         @Override
+         public void write(char[] cbuf, int off, int len) {
+            // Do nothing
+         }
 
-			@Override
-			public void flush() {
-				// Do nothing
-			}
+         @Override
+         public void flush() {
+            // Do nothing
+         }
 
-			@Override
-			public void close() {
-				// Do nothing
-			}
-		};
+         @Override
+         public void close() {
+            // Do nothing
+         }
+      };
 
-		try {
-			stripper.writeText(document, writer);
-		}
-		catch (IOException e) {
-			throw new DocumentProcessingException(e);
-		}
+      try {
+         stripper.writeText(document, writer);
+      }
+      catch (IOException e) {
+         throw new DocumentProcessingException(e);
+      }
 
-		return results;
-	}
+      return results;
+   }
 
-	@Override
-	public void close() {
-		try {
-			document.close();
-		}
-		catch (IOException e) {
-			throw new DocumentProcessingException(e);
-		}
-	}
+   @Override
+   public void close() {
+      try {
+         document.close();
+      }
+      catch (IOException e) {
+         throw new DocumentProcessingException(e);
+      }
+   }
 
-	private Rectangle2D calculateMarkerPosition(String searchText, String snippetText, List<TextPosition> textPositions) {
-		final int textPositionStartIndex = calculateTextPositionStartIndex(searchText, snippetText, textPositions);
+   private Rectangle2D calculateMarkerPosition(String searchText, String snippetText, List<TextPosition> textPositions) {
+      final int textPositionStartIndex = calculateTextPositionStartIndex(searchText, snippetText, textPositions);
 
-		float x1 = Float.MAX_VALUE;
-		float x2 = 0;
-		float y1 = Float.MAX_VALUE;
-		float y2 = 0;
+      float x1 = Float.MAX_VALUE;
+      float x2 = 0;
+      float y1 = Float.MAX_VALUE;
+      float y2 = 0;
 
-		for (int textPositionIndex = textPositionStartIndex; textPositionIndex < textPositionStartIndex + searchText.length(); textPositionIndex++) {
-			TextPosition position = textPositions.get(textPositionIndex);
+      for (int textPositionIndex = textPositionStartIndex; textPositionIndex < textPositionStartIndex + searchText.length(); textPositionIndex++) {
+         TextPosition position = textPositions.get(textPositionIndex);
 
-			x1 = Math.min(x1, position.getXDirAdj());
-			x2 = Math.max(x2, position.getXDirAdj() + position.getWidth());
-			y1 = Math.min(y1, position.getYDirAdj() - position.getHeight());
-			y2 = Math.max(y2, position.getYDirAdj());
-		}
+         x1 = Math.min(x1, position.getXDirAdj());
+         x2 = Math.max(x2, position.getXDirAdj() + position.getWidth());
+         y1 = Math.min(y1, position.getYDirAdj() - position.getHeight());
+         y2 = Math.max(y2, position.getYDirAdj());
+      }
 
-		x1 -= 2;
-		x2 += 2;
-		y1 -= 2;
-		y2 += 2;
+      x1 -= 2;
+      x2 += 2;
+      y1 -= 2;
+      y2 += 2;
 
-		return new Rectangle2D(x1, y1, x2 - x1, y2 - y1);
-	}
+      return new Rectangle2D(x1, y1, x2 - x1, y2 - y1);
+   }
 
-	/**
-	 * Note that number of textPositions might not be equal to the length of the snippetText.
-	 * so we need to account for that.
-	 *
-	 * See: org.apache.pdfbox.text.PDFTextStripper.WordWithTextPositions
-	 */
-	private int calculateTextPositionStartIndex(String searchText, String snippetText, List<TextPosition> textPositions) {
+   /**
+    * Note that number of textPositions might not be equal to the length of the snippetText.
+    * so we need to account for that.
+    *
+    * See: org.apache.pdfbox.text.PDFTextStripper.WordWithTextPositions
+    */
+   private int calculateTextPositionStartIndex(String searchText, String snippetText, List<TextPosition> textPositions) {
 
-		final int snippetTextStartIndex = snippetText.toLowerCase().indexOf(searchText.toLowerCase());
+      final int snippetTextStartIndex = snippetText.toLowerCase().indexOf(searchText.toLowerCase());
 
-		int startIndexDecreaseDelta = 0;
+      int startIndexDecreaseDelta = 0;
 
-		// If any TextPosition (up to the snippetTextStartIndex) contains more then one character, we have to account for that.
-		for (int i=0; i < snippetTextStartIndex; i++) {
-			int numberOfCharactersInTextPosition = textPositions.get(i).getUnicode().length();
-			if (numberOfCharactersInTextPosition > 1) {
-				startIndexDecreaseDelta = startIndexDecreaseDelta + (numberOfCharactersInTextPosition - 1);
-			}
-		}
+      // If any TextPosition (up to the snippetTextStartIndex) contains more then one character, we have to account for that.
+      for (int i=0; i < snippetTextStartIndex; i++) {
+         int numberOfCharactersInTextPosition = textPositions.get(i).getUnicode().length();
+         if (numberOfCharactersInTextPosition > 1) {
+            startIndexDecreaseDelta = startIndexDecreaseDelta + (numberOfCharactersInTextPosition - 1);
+         }
+      }
 
-		return snippetTextStartIndex - startIndexDecreaseDelta;
-	}
+      return snippetTextStartIndex - startIndexDecreaseDelta;
+   }
 
 }
