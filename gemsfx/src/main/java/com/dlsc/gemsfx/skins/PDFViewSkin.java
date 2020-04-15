8964346bcc5050dc2d5023b64e2fad5480827ee1
PrimosK <primoz.kokol@gmail.com>
2020-04-15 15:18:03 +0200

    Set current page number to 0 when document is un-loaded.

--- gemsfx/src/main/java/com/dlsc/gemsfx/skins/PDFViewSkin.java
+++ gemsfx/src/main/java/com/dlsc/gemsfx/skins/PDFViewSkin.java
@@ -341,12 +341,8 @@ public class PDFViewSkin extends SkinBase<PDFView> {
         pageField.setMaxHeight(Double.MAX_VALUE);
         pageField.setAlignment(Pos.CENTER);
         pageField.setMinimumValue(0);
-        pageField.setValue(0);
-        view.pageProperty().addListener(it -> {
-                    pageField.setMinimumValue(1);
-                    pageField.setValue(view.getPage() + 1);
-                }
-        );
+        updateCurrentPageNumber(view, pageField);
+        view.pageProperty().addListener(it -> updateCurrentPageNumber(view, pageField));
         pageField.valueProperty().addListener(it -> {
             final Integer value = pageField.getValue();
             if (value != null) {
@@ -496,6 +492,16 @@ public class PDFViewSkin extends SkinBase<PDFView> {
         PDFView.Document document = getSkinnable().getDocument();
         if (document != null) {
             pageField.setMaximumValue(document.getNumberOfPages());
+        }
+    }
+
+    private void updateCurrentPageNumber(PDFView view, IntegerInputField pageField) {
+        if (view.getDocument() != null) {
+            pageField.setMinimumValue(1);
+            pageField.setValue(view.getPage() + 1);
+        } else {
+            pageField.setMinimumValue(0);
+            pageField.setValue(0);
         }
     }
 
