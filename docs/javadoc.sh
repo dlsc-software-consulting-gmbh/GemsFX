0bf5f661854d120dabb3dc7e7b643c928f271ff9
Dirk Lemmermann <dlemmermann@gmail.com>
2022-10-12 17:46:42 +0200

    Added javadocs to "docs" folder for GitHub pages.

---
+++ docs/javadoc.sh
@@ -0,0 +1,1 @@
+/Library/Java/JavaVirtualMachines/jdk-11.jdk/Contents/Home/bin/javadoc -J-Djavafx.javadoc=true @options @packages @argfile
---
+++ docs/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.dlsc.gemsfx"}]
