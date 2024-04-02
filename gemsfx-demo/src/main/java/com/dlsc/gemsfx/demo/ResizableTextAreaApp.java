06bf56aa9e1af0f6b169b8c4afbf8542bcd131b5
Dirk Lemmermann <dlemmermann@gmail.com>
2024-04-02 09:20:44 +0200

    Refactor code for resizable text area feature
    The code for resizable text area is refactored to include a standard TextArea object for comparison purposes. At the same time, the use and functionality of PseudoClass for focused state is implemented in both TextArea and ResizableTextArea objects.

--- gemsfx-demo/src/main/java/com/dlsc/gemsfx/demo/ResizableTextAreaApp.java
+++ gemsfx-demo/src/main/java/com/dlsc/gemsfx/demo/ResizableTextAreaApp.java
@@ -2,11 +2,13 @@ package com.dlsc.gemsfx.demo;
 
 import com.dlsc.gemsfx.ResizableTextArea;
 import javafx.application.Application;
+import javafx.css.PseudoClass;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
@@ -17,20 +19,26 @@ public class ResizableTextAreaApp extends Application {
 
     @Override
     public void start(Stage stage) {
-        ResizableTextArea textArea = new ResizableTextArea();
+        TextArea textArea = new TextArea("Standard text area ...");
         textArea.setWrapText(true);
         textArea.setMinHeight(100);
+        textArea.pseudoClassStateChanged(PseudoClass.getPseudoClass("focused"), true);
 
-        VBox box = new VBox(textArea);
+        ResizableTextArea resizableTextArea = new ResizableTextArea("Resizable text area ...");
+        resizableTextArea.setWrapText(true);
+        resizableTextArea.setMinHeight(100);
+        resizableTextArea.pseudoClassStateChanged(PseudoClass.getPseudoClass("focused"), true);
+
+        VBox box = new VBox(40, textArea, resizableTextArea);
         box.setFillWidth(false);
         box.setPadding(new Insets(50));
         box.setAlignment(Pos.CENTER);
 
         CheckBox vCheck = new CheckBox("Vertical");
-        vCheck.selectedProperty().bindBidirectional(textArea.resizeVerticalProperty());
+        vCheck.selectedProperty().bindBidirectional(resizableTextArea.resizeVerticalProperty());
 
         CheckBox hCheck = new CheckBox("Horizontal");
-        hCheck.selectedProperty().bindBidirectional(textArea.resizeHorizontalProperty());
+        hCheck.selectedProperty().bindBidirectional(resizableTextArea.resizeHorizontalProperty());
 
         HBox optionsBox = new HBox(10, new Label("Resizable:"), vCheck, hCheck);
         optionsBox.setAlignment(Pos.CENTER);
