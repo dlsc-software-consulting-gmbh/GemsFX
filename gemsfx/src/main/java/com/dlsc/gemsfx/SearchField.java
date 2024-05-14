14d3ecc3511eb877ed904897cf194da60e6b172c
Dirk Lemmermann <dlemmermann@gmail.com>
2024-05-14 10:48:38 +0200

    Updated SearchField documentation for clarity
    The documentation in SearchField.java has been improved for better comprehension of the class's functionality. More detailed descriptions have been added for the critical configuration elements like Suggestion Provider, Converter, Cell Factory, Matcher, New Item Producer, and Comparator.

--- gemsfx/src/main/java/com/dlsc/gemsfx/SearchField.java
+++ gemsfx/src/main/java/com/dlsc/gemsfx/SearchField.java
@@ -57,7 +57,7 @@ import java.util.function.Consumer;
 /**
  * The search field is a standard text field with auto suggest capabilities
  * and a selection model for a specific type of object. This type is defined by the
- * generic type argument. The main difference to other auto suggest text fields is that
+ * generic type argument. The main difference to other auto-suggest text fields is that
  * the main outcome of this field is an object and not just the text entered by the
  * user. Another difference is how the text field automatically finds and selects the
  * first object that matches the text typed by the user so far. A third feature of
@@ -66,8 +66,17 @@ import java.util.function.Consumer;
  * provider. This last feature allows an application to let the user either pick an
  * existing object or to create a new one on-the-fly (but only if a new item producer
  * has been set).
+ * <p>
+ * The search field requires proper configuration to work correctly:
  *
- * <h3>Matcher</h3>
+ * <ol>
+ * <li><b>Suggestion Provider</b> - a callback that returns a collection of items for a given search field suggestion request. The suggestion provider is invoked asynchronously via JavaFX concurrency API (service & task). The suggestion provider gets invoked slightly delayed whenever the user types some text into the field. If the user types again the current search gets cancelled and a new search gets initiated. As long as the user types fast enough the actual search will not be performed.</li>
+ * <li><b>Converter</b> - the converter is used to convert the items found in the suggestions list to text. This is just a standard StringConverter instance (only the toString() method needs to be implemented).</li>
+ * <li><b>Cell Factory</b> - a standard list cell factory / callback used for the ListView instance shown in the popup that presents the suggested items. The default cell factory should be sufficient for most use cases. It simply displays the name of the items via the help of the string converter. However, it also underlines the text match in the name.</li>
+ * <li><b>Matcher</b> - a function taking two arguments that will be applied to the suggested items to find "perfect matches" for the given search text (entered by the user). The function takes an item and the search text as input and returns a boolean. The first perfect match found will be used to autocomplete the text of the search field.</li>
+ * <li><b>New Item Producer</b> - a callback that returns a new item instance of the type supported by the search field. This callback is used if the field is configured to create items "on-the-fly", meaning the typed text does not match anything in the suggested list of items.</li>
+ * <li><b>Comparator</b> - a standard comparator used to perform a first sorting of the suggested items. However, internally the field wraps this comparator to place some items higher up in the dropdown list as they are better matches for the current search text.</li>
+ * </ol>
  *
  * @param <T> the type of objects to work on
  * @see #setSuggestionProvider(Callback)
