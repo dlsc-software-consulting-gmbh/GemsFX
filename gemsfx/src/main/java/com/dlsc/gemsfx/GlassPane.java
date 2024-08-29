296ea5d496162c149df4f222a95d5889c1d40666
Dirk Lemmermann <dlemmermann@gmail.com>
2024-08-29 16:56:06 +0200

    Added missing API to the GlassPane control.

--- gemsfx/src/main/java/com/dlsc/gemsfx/GlassPane.java
+++ gemsfx/src/main/java/com/dlsc/gemsfx/GlassPane.java
@@ -13,7 +13,8 @@ import javafx.util.Duration;
 
 /**
  * A simple pane that can be used to overlay the UI with a semi-transparent color,
- * indicating that input is blocked.
+ * indicating that input is blocked. The glass pane can be animated, which means it
+ * will fade in / fade out when it becomes visible or invisible.
  */
 public class GlassPane extends StackPane {
 
@@ -64,6 +65,13 @@ public class GlassPane extends StackPane {
         return blockingOpacity.get();
     }
 
+    /**
+     * The opacity value between 0 and 1 that will be used to gray out the
+     * content over which the glass pane is place. A separate opacity property is needed
+     * to support the fade in / fade out animation AND the regular opacity.
+     *
+     * @return the opacity of the glass pane
+     */
     public final DoubleProperty blockingOpacityProperty() {
         return blockingOpacity;
     }
@@ -78,6 +86,11 @@ public class GlassPane extends StackPane {
         return fadeInOutDuration.get();
     }
 
+    /**
+     * Stores the duration of the fade in / fade out animation.
+     *
+     * @return the animation duration in milliseconds
+     */
     public final ObjectProperty<Duration> fadeInOutDurationProperty() {
         return fadeInOutDuration;
     }
@@ -92,14 +105,34 @@ public class GlassPane extends StackPane {
         return fadeInOut.get();
     }
 
+    /**
+     * A property that determines whether we want to use a fade in / out animation of the glass pane
+     * when it gets shown or hidden.
+     *
+     * @see #fadeInOutDurationProperty()
+     * @return true if the fade in / out process will be animated
+     */
     public final BooleanProperty fadeInOutProperty() {
         return fadeInOut;
     }
 
+    public final void setFadeInOut(boolean fadeInOut) {
+        this.fadeInOut.set(fadeInOut);
+    }
+
     private final BooleanProperty hide = new SimpleBooleanProperty(this, "hide", true);
 
+    /**
+     * Controls whether the glass pane is hidden or shown.
+     *
+     * @return true if the pane is currently hidden
+     */
     public final BooleanProperty hideProperty() {
         return hide;
+    }
+
+    public final void setHide(boolean hide) {
+        this.hide.set(hide);
     }
 
     public final boolean isHide() {
