75962a0b30f570ea9ebfe0e16bef90b0717a68c5
Dirk Lemmermann <dlemmermann@gmail.com>
2025-02-23 12:24:56 +0100

    Remove redundant stylesheet additions in UI components
    The `getUserAgentStylesheet()` calls were removed from multiple component initializations as they were unnecessary. This helps simplify the code and avoids potential redundant styling application.

--- gemsfx/src/main/java/com/dlsc/gemsfx/SimpleFilterView.java
+++ gemsfx/src/main/java/com/dlsc/gemsfx/SimpleFilterView.java
@@ -284,7 +284,6 @@ public class SimpleFilterView extends HBox {
      */
     public final <T> SelectionBox<T> addSelectionBox(String text) {
         SelectionBox<T> box = createSelectionBox();
-        box.getStylesheets().add(getUserAgentStylesheet());
         box.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         box.setPromptText(text);
         box.setMaxWidth(Double.MAX_VALUE);
@@ -365,7 +364,6 @@ public class SimpleFilterView extends HBox {
      */
     public final DateRangePicker addDateRangePicker(String text) {
         DateRangePicker dateRangePicker = createDateRangePicker();
-        dateRangePicker.getStylesheets().add(getUserAgentStylesheet());
         dateRangePicker.setValue(null);
         dateRangePicker.setShowPresetTitle(false);
         dateRangePicker.setPromptText(text);
@@ -421,7 +419,6 @@ public class SimpleFilterView extends HBox {
      */
     public final CalendarPicker addCalendarPicker(String text) {
         CalendarPicker datePicker = createCalendarPicker();
-        datePicker.getStylesheets().add(getUserAgentStylesheet());
         datePicker.setEditable(false);
         datePicker.setValue(null);
         datePicker.setPromptText(text);
@@ -479,7 +476,6 @@ public class SimpleFilterView extends HBox {
      */
     public final DatePicker addDatePicker(String text) {
         DatePicker datePicker = createDatePicker();
-        datePicker.getStylesheets().add(getUserAgentStylesheet());
         datePicker.setEditable(false);
         datePicker.setValue(null);
         datePicker.setPromptText(text);
