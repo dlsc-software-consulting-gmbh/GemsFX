79600f766065fb81523baa647bdd3faf007cc96f
Dirk Lemmermann <dlemmermann@gmail.com>
2025-06-04 12:41:43 +0200

    Add horizontal content bias to `GridTableView`
    Implemented `getContentBias` to return `Orientation.HORIZONTAL`, enabling improved layout behavior and better alignment for horizontal content.

--- gemsfx/src/main/java/com/dlsc/gemsfx/gridtable/GridTableView.java
+++ gemsfx/src/main/java/com/dlsc/gemsfx/gridtable/GridTableView.java
@@ -14,6 +14,7 @@ import javafx.beans.property.SimpleLongProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Orientation;
 import javafx.scene.Node;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
@@ -41,6 +42,11 @@ public class GridTableView<S> extends Control {
                 requestFocus();
             }
         });
+    }
+
+    @Override
+    public Orientation getContentBias() {
+        return Orientation.HORIZONTAL;
     }
 
     @Override
