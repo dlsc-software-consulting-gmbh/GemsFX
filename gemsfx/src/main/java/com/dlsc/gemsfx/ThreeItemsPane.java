e97beb2b3d2da7913d78bfbe2976faa344b37c9f
Dirk Lemmermann <dlemmermann@gmail.com>
2025-09-10 11:27:41 +0200

    Add Javadoc comments and `getContentBias` implementation to `ThreeItemsPane` for improved documentation and layout orientation handling

--- gemsfx/src/main/java/com/dlsc/gemsfx/ThreeItemsPane.java
+++ gemsfx/src/main/java/com/dlsc/gemsfx/ThreeItemsPane.java
@@ -10,8 +10,34 @@ import javafx.geometry.Orientation;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 
+import java.util.List;
+
+/**
+ * A custom layout container that arranges up to three child nodes in either a horizontal
+ * or vertical orientation. This pane allows spacing between the nodes and provides methods
+ * to control the alignment and orientation of the child nodes.
+ *
+ * The container manages three possible child nodes, identified as item1, item2, and item3.
+ * The layout updates dynamically whenever the nodes or properties such as orientation or
+ * spacing are modified.
+ *
+ * Features include:
+ * - Dynamic management of child nodes: up to three nodes can be added and arranged.
+ * - Adjustable orientation: supports horizontal and vertical alignment through the
+ *   orientation property.
+ * - Customizable spacing: allows setting the spacing between child nodes.
+ *
+ * Override methods provide computed sizes for use during layouts, including preferred,
+ * minimum, and maximum widths and heights.
+ */
 public class ThreeItemsPane extends Pane {
 
+    private Orientation bias;
+    private boolean biasDirty = true;
+
+    /**
+     * Constructs a new instance of the ThreeItemsPane class.
+     */
     public ThreeItemsPane() {
         InvalidationListener updateChildrenListener = it -> updateChildren();
         item1.addListener(updateChildrenListener);
@@ -19,6 +45,34 @@ public class ThreeItemsPane extends Pane {
         item3.addListener(updateChildrenListener);
 
         orientation.addListener(it -> requestLayout());
+    }
+
+    @Override public void requestLayout() {
+        biasDirty = true;
+        bias = null;
+        super.requestLayout();
+    }
+
+    /**
+     *
+     * @return null unless one of its children has a content bias.
+     */
+    @Override public Orientation getContentBias() {
+        if (biasDirty) {
+            bias = null;
+            final List<Node> children = getManagedChildren();
+            for (Node child : children) {
+                Orientation contentBias = child.getContentBias();
+                if (contentBias != null) {
+                    bias = contentBias;
+                    if (contentBias == Orientation.HORIZONTAL) {
+                        break;
+                    }
+                }
+            }
+            biasDirty = false;
+        }
+        return bias;
     }
 
     private void updateChildren() {
@@ -37,11 +91,18 @@ public class ThreeItemsPane extends Pane {
 
     private final ObjectProperty<Orientation> orientation = new SimpleObjectProperty<>(Orientation.HORIZONTAL);
 
-    public Orientation getOrientation() {
+    public final Orientation getOrientation() {
         return orientation.get();
     }
 
-    public ObjectProperty<Orientation> orientationProperty() {
+    /**
+     * Returns the property object for the orientation of the ThreeItemsPane.
+     * This property determines whether the layout of the items is horizontal
+     * or vertical.
+     *
+     * @return the orientation property
+     */
+    public final ObjectProperty<Orientation> orientationProperty() {
         return orientation;
     }
 
@@ -51,57 +112,75 @@ public class ThreeItemsPane extends Pane {
 
     private final ObjectProperty<Node> item1 = new SimpleObjectProperty<>(this, "item1");
 
-    public Node getItem1() {
+    public final Node getItem1() {
         return item1.get();
     }
 
-    public ObjectProperty<Node> item1Property() {
+    /**
+     * Returns the property object for the first item in the ThreeItemsPane.
+     * This property represents the first Node managed by the pane.
+     *
+     * @return the property object representing the first item Node
+     */
+    public final ObjectProperty<Node> item1Property() {
         return item1;
     }
 
-    public void setItem1(Node item1) {
+    public final void setItem1(Node item1) {
         this.item1.set(item1);
     }
 
     private final ObjectProperty<Node> item2 = new SimpleObjectProperty<>(this, "item2");
 
-    public Node getItem2() {
+    public final Node getItem2() {
         return item2.get();
     }
 
-    public ObjectProperty<Node> item2Property() {
+    /**
+     * Returns the property object for the second item in the ThreeItemsPane.
+     * This property represents the second Node managed by the pane.
+     *
+     * @return the property object representing the second item Node
+     */
+    public final ObjectProperty<Node> item2Property() {
         return item2;
     }
 
-    public void setItem2(Node item2) {
+    public final void setItem2(Node item2) {
         this.item2.set(item2);
     }
 
     private final ObjectProperty<Node> item3 = new SimpleObjectProperty<>(this, "item3");
 
-    public Node getItem3() {
+    public final Node getItem3() {
         return item3.get();
     }
 
-    public ObjectProperty<Node> item3Property() {
+    /**
+     * Returns the property object for the third item in the ThreeItemsPane.
+     * This property represents the third Node managed by the pane.
+     *
+     * @return the property object representing the first item Node
+     */
+    public final ObjectProperty<Node> item3Property() {
         return item3;
     }
 
-    public void setItem3(Node item3) {
+    public final void setItem3(Node item3) {
         this.item3.set(item3);
     }
 
     private final DoubleProperty spacing = new SimpleDoubleProperty(this, "spacing", 0);
 
-    public double getSpacing() {
+    public final double getSpacing() {
         return spacing.get();
     }
 
-    public DoubleProperty spacingProperty() {
+    public final DoubleProperty spacingProperty() {
         return spacing;
     }
 
-    public void setSpacing(double spacing) {
+    public final void setSpacing(double spacing) {
         this.spacing.set(spacing);
     }
 
@@ -127,9 +206,11 @@ public class ThreeItemsPane extends Pane {
         if (getItem1() != null) {
             h1 = getItem1().prefHeight(width - getInsets().getLeft() - getInsets().getRight());
         }
+
         if (getItem2() != null) {
             h2 = getItem2().prefHeight(width - getInsets().getLeft() - getInsets().getRight());
         }
+
         if (getItem3() != null) {
             h3 = getItem3().prefHeight(width - getInsets().getLeft() - getInsets().getRight());
         }
@@ -150,9 +231,11 @@ public class ThreeItemsPane extends Pane {
         if (getItem1() != null) {
             h1 = getItem1().minHeight(width - getInsets().getLeft() - getInsets().getRight());
         }
+
         if (getItem2() != null) {
             h2 = getItem2().minHeight(width - getInsets().getLeft() - getInsets().getRight());
         }
+
         if (getItem3() != null) {
             h3 = getItem3().minHeight(width - getInsets().getLeft() - getInsets().getRight());
         }
@@ -173,9 +256,11 @@ public class ThreeItemsPane extends Pane {
         if (getItem1() != null) {
             h1 = getItem1().maxHeight(width - getInsets().getLeft() - getInsets().getRight());
         }
+
         if (getItem2() != null) {
             h2 = getItem2().maxHeight(width - getInsets().getLeft() - getInsets().getRight());
         }
+
         if (getItem3() != null) {
             h3 = getItem3().maxHeight(width - getInsets().getLeft() - getInsets().getRight());
         }
@@ -196,9 +281,11 @@ public class ThreeItemsPane extends Pane {
         if (getItem1() != null) {
             w1 = getItem1().prefWidth(height - getInsets().getTop() - getInsets().getBottom());
         }
+
         if (getItem2() != null) {
             w2 = getItem2().prefWidth(height - getInsets().getTop() - getInsets().getBottom());
         }
+
         if (getItem3() != null) {
             w3 = getItem3().prefWidth(height - getInsets().getTop() - getInsets().getBottom());
         }
@@ -219,9 +306,11 @@ public class ThreeItemsPane extends Pane {
         if (getItem1() != null) {
             w1 = getItem1().minWidth(height - getInsets().getTop() - getInsets().getBottom());
         }
+
         if (getItem2() != null) {
             w2 = getItem2().minWidth(height - getInsets().getTop() - getInsets().getBottom());
         }
+
         if (getItem3() != null) {
             w3 = getItem3().minWidth(height - getInsets().getTop() - getInsets().getBottom());
         }
@@ -242,9 +331,11 @@ public class ThreeItemsPane extends Pane {
         if (getItem1() != null) {
             w1 = getItem1().maxWidth(height - getInsets().getTop() - getInsets().getBottom());
         }
+
         if (getItem2() != null) {
             w2 = getItem2().maxWidth(height - getInsets().getTop() - getInsets().getBottom());
         }
+
         if (getItem3() != null) {
             w3 = getItem3().maxWidth(height - getInsets().getTop() - getInsets().getBottom());
         }
